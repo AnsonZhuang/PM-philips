037a540205c43f3666aa34ac89e16e3ec930b478
AnsonZhuang <1742547552@qq.com>
2023-03-20 06:00:39 +0800

    20230319 1 Finish config.py & .env 2 Finish models.py 3 Finish register & login (Not yet reset pwd) 4 Working on index 5 Working on personal info

--- apps/user/routes.py
+++ apps/user/routes.py
@@ -3,6 +3,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from apps import db
 from apps.user import blueprint
 from flask import render_template, request
 from flask_login import login_required
@@ -26,88 +27,36 @@ def my_projects():
 def my_team():
     return render_template('user/my_team.html')
 
-# @blueprint.route('/index')
-# @login_required
-# def index():
-#     return render_template('pages/index.html', segment='index')
+
+@blueprint.route('/create-example-project')
+@login_required
+def create_example_project():
+    project_id = 15
+    project_name = 'example_project'
+    create_resource_table(project_id, project_name)
+    return render_template('user/my_projects.html')
 
 
-# @blueprint.route('/typography')
-# @login_required
-# def typography():
-#     return render_template('pages/typography.html')
-#
-# @blueprint.route('/color')
-# @login_required
-# def color():
-#     return render_template('pages/color.html')
-#
-# @blueprint.route('/icon-tabler')
-# @login_required
-# def icon_tabler():
-#     return render_template('pages/icon-tabler.html')
-#
-# @blueprint.route('/sample-page')
-# @login_required
-# def sample_page():
-#     return render_template('pages/sample-page.html')
-#
-# @blueprint.route('/accounts/password-reset/')
-# def password_reset():
-#     return render_template('accounts/password_reset.html')
-#
-# @blueprint.route('/accounts/password-reset-done/')
-# def password_reset_done():
-#     return render_template('accounts/password_reset_done.html')
-#
-# @blueprint.route('/accounts/password-reset-confirm/')
-# def password_reset_confirm():
-#     return render_template('accounts/password_reset_confirm.html')
-#
-# @blueprint.route('/accounts/password-reset-complete/')
-# def password_reset_complete():
-#     return render_template('accounts/password_reset_complete.html')
-#
-# @blueprint.route('/accounts/password-change/')
-# def password_change():
-#     return render_template('accounts/password_change.html')
-#
-# @blueprint.route('/accounts/password-change-done/')
-# def password_change_done():
-#     return render_template('accounts/password_change_done.html')
-#
-# @blueprint.route('/<template>')
-# @login_required
-# def route_template(template):
-#
-#     try:
-#
-#         if not template.endswith('.html'):
-#             template += '.html'
-#
-#         # Detect the current page
-#         segment = get_segment(request)
-#
-#         # Serve the file (if exists) from app/templates/home/FILE.html
-#         return render_template("home/" + template, segment=segment)
-#
-#     except TemplateNotFound:
-#         return render_template('home/page-404.html'), 404
-#
-#     except:
-#         return render_template('home/page-500.html'), 500
-#
-# # Helper - Extract current page name from request
-# def get_segment(request):
-#
-#     try:
-#
-#         segment = request.path.split('/')[-1]
-#
-#         if segment == '':
-#             segment = 'index'
-#
-#         return segment
-#
-#     except:
-#         return None
+def create_resource_table(project_id, project_name):
+    table_name = "project_resource_" + project_id
+    columns = {
+        "__tablename__": table_name,
+        "id": db.Column(db.Integer, primary_key=True, autoincrement=False),
+        "Year": db.Column(db.Integer),
+        "Jan": db.Column(db.Integer),
+        "Feb": db.Column(db.Integer),
+        "Mar": db.Column(db.Integer),
+        "Apr": db.Column(db.Integer),
+        "May": db.Column(db.Integer),
+        "Jun": db.Column(db.Integer),
+        "Jul": db.Column(db.Integer),
+        "Aug": db.Column(db.Integer),
+        "Sep": db.Column(db.Integer),
+        "Oct": db.Column(db.Integer),
+        "Nov": db.Column(db.Integer),
+        "Dec": db.Column(db.Integer),
+    }
+    # 由于使用flask migrate 必须创建对应模型来创建表
+    new_model = type(project_name, (db.Model,), columns)
+    db.create_all()
+    return table_name
